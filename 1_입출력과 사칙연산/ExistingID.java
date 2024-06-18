0be1f93e2e60dfc82ad314adaf2cc4ae9a10467c
Yeonho Lee <6464106@snu.ac.kr>
2024-06-19 01:58:57 +0900

    1일차

---
+++ 1_입출력과 사칙연산/ExistingID.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ExistingID {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String ID = scanner.next();
+
+        System.out.println(ID + "??!");
+    }
+}
---
+++ 1_입출력과 사칙연산/Print_AB.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Print_AB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println((double)A / B);
+    }
+}
